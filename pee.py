91cd8d3c8c301fb48409a3e6ec0abc31eae60265
201585052 <1216884492@qq.com>
2018-10-10 22:39:56 +0800

    peech

--- pee.py
+++ pee.py
@@ -4,12 +4,9 @@ import sys
 import os
 import numpy as np
 
-import h5py
-import cv2
-import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt
-
+import matplotlib.image as pimg
+from PIL import Image
 import paddle
 import paddle.fluid as fluid
 from paddle.v2.plot import Ploter
@@ -22,52 +19,52 @@ trainlabel_org = []
 testset_org = []
 testlabel_org = []
 
+def cutImage(img):
+    width,height = img.size
+    return img.resize(((int)(0.4 * width),(int)(0.4 * height)))
 def getData(str,x,y,val):
-	cnt = 0 # 明白了这里之所以会killed掉的原因就是数据量太大算力不够，姑且弄成100
-	for filename in os.listdir(str):
-		x.append(cv2.imread(str + '/' + filename))
-		y.append(val)
-		cnt = cnt + 1
-		if cnt >= 100:
-			break
+    for filename in os.listdir(str):
+        x.append(np.array(cutImage(Image.open(str + '/' + filename))).flatten()) # 不是数据算力的问题 可以把图片变小
+        y.append(np.array(val))
+
 
 # 读取所有的数据包括训练集和测试集
-getData("../train/R0",trainset_org,trainlabel_org,0)
-getData("../train/B1",trainset_org,trainlabel_org,1)
-getData("../train/M2",trainset_org,trainlabel_org,2)
-getData("../train/S3",trainset_org,trainlabel_org,3)
-getData("../test/R0",testset_org,testlabel_org,0)
-getData("../test/B1",testset_org,testlabel_org,1)
-getData("../test/M2",testset_org,testlabel_org,2)
-getData("../test/S3",testset_org,testlabel_org,3)
+getData("/home/aistudio/petch/peech/train/R0",trainset_org,trainlabel_org,0)
+getData("/home/aistudio/petch/peech/train/B1",trainset_org,trainlabel_org,1)
+getData("/home/aistudio/petch/peech/train/M2",trainset_org,trainlabel_org,2)
+getData("/home/aistudio/petch/peech/train/S3",trainset_org,trainlabel_org,3)
+getData("/home/aistudio/petch/peech/test/R0",testset_org,testlabel_org,0)
+getData("/home/aistudio/petch/peech/test/B1",testset_org,testlabel_org,1)
+getData("/home/aistudio/petch/peech/test/M2",testset_org,testlabel_org,2)
+getData("/home/aistudio/petch/peech/test/S3",testset_org,testlabel_org,3)
 
-# 先对图片数据进行处理
+# trainset的左右侧
 trainset_org = np.array(trainset_org)
-m_train = trainset_org.shape[0]
+trainlabel_org = np.array(trainlabel_org).reshape(-1,1)
 
-testset_org = np.array(testset_org) 
-m_test = testset_org.shape[0]
-num_px = testset_org.shape[1]
+# testset的左右侧
+testset_org = np.array(testset_org)
+testlabel_org = np.array(testlabel_org).reshape(-1,1)
 
-DATA_DIM = num_px * num_px * 15 # 定义维度,这里应该出了问题
+# 归一化
+trainset_org = trainset_org/255.
+testset_org = testset_org/255.
 
-trainset_flatten = trainset_org.reshape(m_train,-1) # 训练集降维
-testset_flatten = testset_org.reshape(m_test,-1) # 测试集降维
-trainset_x = trainset_flatten/255. # 训练集归一化
-testset_x = testset_flatten/255. # 测试集归一化
+# print trainset_org
 
-# 再对label进行处理
-trainlabel_org = np.array(trainlabel_org)
-trainset_y = np.array([trainlabel_org])
+train_set = np.hstack((trainset_org, trainlabel_org))
+test_set = np.hstack((testset_org, testlabel_org))
 
-testlabel_org = np.array(testlabel_org)
-testset_y = np.array([testlabel_org])
+DATA_DIM = train_set.shape[1] - 1
+print DATA_DIM
 
-# 接下来看数组横向合并，如果出问题可能是对label的处理不对，我这个没省略号。。
-train_set = np.hstack((trainset_x,trainset_y.T))
-test_set = np.hstack((testset_x,testset_y.T))
-
-# 至此基本材料都应该准备好了
+train_set = train_set.reshape(-1,train_set.shape[1])
+np.random.shuffle(train_set) # 随机分布很重要
+test_set = test_set.reshape(-1,test_set.shape[1])
+np.random.shuffle(test_set) # 同上
+# 至此唯一的问题好像是np.array的问题
+print train_set.shape
+print train_set
 
 # 定义reader
 
@@ -102,7 +99,9 @@ place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
 
 def infer_func():
     x = fluid.layers.data(name='x', shape=[DATA_DIM], dtype='float32')
-    y_predict = fluid.layers.fc(input=x, size=2, act='softmax')
+    h1 = fluid.layers.fc(input=x, size=16, act='relu')
+    h2 = fluid.layers.fc(input=h1,size=32, act='relu')
+    y_predict = fluid.layers.fc(input=h2, size=4, act='softmax')
     return y_predict
 
 feeder = None
@@ -131,18 +130,18 @@ trainer = fluid.Trainer(
 
 feed_order=['x','y']
 
-BATCH_SIZE=4
+BATCH_SIZE=40
 
 # 设置训练reader
 train_reader = paddle.batch(
     paddle.reader.shuffle(
-        read_data(train_set), buf_size=100),
+        read_data(train_set), buf_size=500),
     batch_size=BATCH_SIZE)
 
 #设置测试 reader
 test_reader = paddle.batch(
     paddle.reader.shuffle(
-        read_data(test_set), buf_size=100),
+        read_data(test_set), buf_size=500),
     batch_size=BATCH_SIZE)
 
 save_dirname="recognize_peech_inference.model"
@@ -183,85 +182,78 @@ def event_handler_plot(event):
    # plot_cost.savefig("./planecost.jpg")
    # 考虑一下图片的实时绘制和保存问题
 
-print "请选择您要进行的操作？0:训练，1:预测"
-choice = input()
-
 # 开始训练了
-if choice == 0:
-    EPOCH_NUM = 20
-    trainer.train(
-        reader=train_reader,
-        num_epochs=EPOCH_NUM,
-        event_handler=event_handler_plot,
-        feed_order=feed_order)
+EPOCH_NUM = 10 # 整体训练轮数
+trainer.train(
+    reader=train_reader,
+    num_epochs=EPOCH_NUM,
+    event_handler=event_handler_plot,
+    feed_order=feed_order)
 
+inferencer = fluid.Inferencer(
+    infer_func=infer_func, param_path=save_dirname, place=place)
 
-# 接下来是预测
-elif choice == 1:
-    inferencer = fluid.Inferencer(
-        infer_func=infer_func, param_path=save_dirname, place=place)
-    BATCH_SIZE = 10
-    test_reader = paddle.batch(
-        read_data(test_set), batch_size=BATCH_SIZE
-    )
+BATCH_SIZE = 10
+test_reader = paddle.batch(
+    read_data(test_set), batch_size=BATCH_SIZE
+)
 
 # 取出一个 mini-batch
-    for mini_batch in test_reader(): 
-        # 转化为 numpy 的 ndarray 结构，并且设置数据类型
+for mini_batch in test_reader(): 
+    # 转化为 numpy 的 ndarray 结构，并且设置数据类型
+    test_x = np.array([data[0] for data in mini_batch]).astype("float32")
+    test_y = np.array([data[1] for data in mini_batch]).astype("int64")
+    # 真实进行预测
+    mini_batch_result = inferencer.infer({'x': test_x})
+    
+    # 打印预测结果
+    mini_batch_result = np.argsort(mini_batch_result) #找出可能性最大的列标，升序排列
+    mini_batch_result = mini_batch_result[0][:, -1]  #把这些列标拿出来
+    print('预测结果：%s'%mini_batch_result)
+    
+    # 打印真实结果    
+    label = np.array(test_y) # 转化为 label
+    print('真实结果：%s'%label)
+    break
+
+# 查看百分比
+def right_ratio(right_counter, total):
+    ratio = float(right_counter)/total
+    return ratio
+
+# 评估函数 data_set 是一个reader
+def evl(data_set):
+    total = 0    #操作的元素的总数
+    right_counter = 0  #正确的元素
+
+    pass_num = 0
+    for mini_batch in data_set():
+        pass_num += 1
+        #预测
         test_x = np.array([data[0] for data in mini_batch]).astype("float32")
         test_y = np.array([data[1] for data in mini_batch]).astype("int64")
-        # 真实进行预测
         mini_batch_result = inferencer.infer({'x': test_x})
         
-        # 打印预测结果
+        #预测的结果
         mini_batch_result = np.argsort(mini_batch_result) #找出可能性最大的列标，升序排列
         mini_batch_result = mini_batch_result[0][:, -1]  #把这些列标拿出来
-        print('预测结果：%s'%mini_batch_result)
-        
-        # 打印真实结果    
+#         print('预测结果：%s'%mini_batch_result)
+
         label = np.array(test_y) # 转化为 label
-        print('真实结果：%s'%label)
-        break
-    
-# 定义评估效果的函数
-    
-    # 查看百分比
-    def right_ratio(right_counter, total):
-        ratio = float(right_counter)/total
-        return ratio
-    
-    def evl(data_set):
-        total = 0    #操作的元素的总数
-        right_counter = 0  #正确的元素
-    
-        pass_num = 0
-        for mini_batch in data_set():
-            pass_num += 1
-            #预测
-            test_x = np.array([data[0] for data in mini_batch]).astype("float32")
-            test_y = np.array([data[1] for data in mini_batch]).astype("int64")
-            mini_batch_result = inferencer.infer({'x': test_x})
-            
-            #预测的结果
-            mini_batch_result = np.argsort(mini_batch_result) #找出可能性最大的列标，升序排列
-            mini_batch_result = mini_batch_result[0][:, -1]  #把这些列标拿出来
-            # print('预测结果：%s'%mini_batch_result)
-    
-            label = np.array(test_y) # 转化为 label
-            # print('真实结果：%s'%label)
-    
-            #计数
-            label_len = len(label)
-            total += label_len
-            for i in xrange(label_len):
-                if mini_batch_result[i] == label[i]:
-                    right_counter += 1
-    
-        ratio = right_ratio(right_counter, total)
-        return ratio
-    
-    ratio = evl(train_reader)
-    print('训练数据的正确率 %0.2f%%'%(ratio*100))
-    
-    ratio = evl(test_reader)
-    print('预测数据的正确率 %0.2f%%'%(ratio*100))
+#         print('真实结果：%s'%label)
+
+        #计数
+        label_len = len(label)
+        total += label_len
+        for i in xrange(label_len):
+            if mini_batch_result[i] == label[i]:
+                right_counter += 1
+
+    ratio = right_ratio(right_counter, total)
+    return ratio
+
+ratio = evl(train_reader)
+print('训练数据的正确率 %0.2f%%'%(ratio*100))
+
+ratio = evl(test_reader)
+print('预测数据的正确率 %0.2f%%'%(ratio*100))
